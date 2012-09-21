b6c971bed48d913153e62f9488ccb5eb86e8876f
Matt Bailey <matt@gpmd.co.uk>
2012-09-21 17:43:58 +0100

    Added license message to all files

--- app/code/local/GPMD/Static/Block/Assets/Includes.php
+++ app/code/local/GPMD/Static/Block/Assets/Includes.php
@@ -1,18 +1,28 @@
 <?php
 /**
  *
+ * This file is part of the GPMD Magento Responsive Theme.
  *
- * @category   GPMD
- * @package    GPMD_Static
- * @author     Carlo Tasca @GPMD LTD
+ * The GPMD Magento Responsive Theme is free software: you can redistribute
+ * it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License,
+ * or (at your option) any later version.
+ *
+ * The GPMD Magento Responsive Theme is distributed in the hope that it will
+ * be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
+ * Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with the GPMD Magento Responsive Theme. If not, see
+ * http://www.gnu.org/licenses/.
+ *
+ * @category    GPMD
+ * @package     GPMD_Responsive_Theme
+ * @copyright   Copyright (c) 2012 GPMD.
+ * @author      Carlo Tasca @GPMD LTD
  */
 
-/**
- *
- * @category   GPMD
- * @package    GPMD_Static
- * @author     Carlo Tasca @GPMD LTD
- */
 class GPMD_Static_Block_Assets_Includes extends Mage_Core_Block_Template 
 {
 	protected $_version = '1.7.1'; // latest version of jQuery at time of writing
